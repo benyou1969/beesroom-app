2632db7bed2e23ee2429a2fafc6cab91ccf4ce8d
benyou1969 <benyou.11@hotmail.com>
2020-09-20 17:12:50 +0100

    feat(api): upload files
    add support to upload images for profile pic

--- apps/api/src/app/models/user/user.resolver.ts
+++ apps/api/src/app/models/user/user.resolver.ts
@@ -1,10 +1,12 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { User } from '../../entities/user.entity';
+import { createWriteStream } from 'fs';
+
 import { GetUserGraphQL } from '../auth/decorators/graphql-get-user.decorators';
 import { GqlAuthGuard } from '../auth/guards/graphql-jwt-auth.guard';
 import { UpdateUserInfo } from './interfaces/update-user-info.input';
-
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
+import { User } from '../../entities/user.entity';
 import { UserService } from './user.service';
 
 @Resolver()
@@ -18,5 +20,19 @@ export class UserResolver {
     @Args('updateUserInfo') updateUserInfo: UpdateUserInfo
   ): Promise<User> {
     return this.userService.updateProfile(user, updateUserInfo);
+  }
+
+  @Mutation(() => Boolean)
+  async uploadFile(
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    { createReadStream, filename }: FileUpload
+  ): Promise<boolean> {
+    console.log(filename);
+    return new Promise(async (resolve, reject) =>
+      createReadStream()
+        .pipe(createWriteStream(`apps/api/src/app/uploads/${filename}`))
+        .on('finish', () => resolve(true))
+        .on('error', () => reject(false))
+    );
   }
 }
